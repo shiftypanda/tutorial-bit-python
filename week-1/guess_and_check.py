4e06aa40cb6268e832a716db9fcea3400fc0310e
Shiftypanda <drtimddavis@gmail.com>
2018-06-24 19:29:52 +0100

    guess n check modified to take negative numbers

--- week-1/guess_and_check.py
+++ week-1/guess_and_check.py
@@ -5,11 +5,13 @@ def guess_and_check_cube_root():
     """Used to guess and check cube root"""
     x = int(input('Enter an integer: '))
     ans = 0
-    while ans**3 < x:     # generates a guess
+    while ans**3 < abs(x):     # generates a guess
         ans = ans + 1
-    if ans**3 != x:
+    if ans**3 != abs(x):
         print(str(x) + ' is not a perfect cube')
     else:
+        if x < 0:
+            ans = - ans
         print('Cube root of ' + str(x) + ' is ' + str(ans))
 
 guess_and_check_cube_root()
