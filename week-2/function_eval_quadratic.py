b0b5a52ee8516fa072787e898726c0f2289bf3f1
Shiftypanda <drtimddavis@gmail.com>
2018-06-26 23:44:04 +0100

    notes added for basic functions and docstrings

---
+++ week-2/function_eval_quadratic.py
@@ -0,0 +1,12 @@
+# Write a Python function, evalQuadratic(a, b, c, x),
+# that returns the value of solved quadratic
+
+def evalQuadtratic(a, b, c, x):
+    """
+    :param a: numerical value for coefficient of a quadratic
+    :param b: ''
+    :param c: ''
+    :param x: numerical value at which to evaluate the quadratic
+    :return:
+    """
+    return (a*(x**2)+(b*x) + c)
