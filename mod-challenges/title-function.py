aea86ddfad8d22ae8403dc779fdbc43e9f78f9bc
Shiftypanda <drtimddavis@gmail.com>
2018-06-30 22:23:08 +0100

    added mod challenge to replicate title function

---
+++ mod-challenges/title-function.py
@@ -0,0 +1,37 @@
+# Make function that replicates functionality of built in python string function.title()
+# Idea created on beginner programmers discord
+# by MajorDiscord and Fisk, Unit tests by Shiftypanda
+from unittest import TestCase
+
+
+s = 'we lOve to hAVe really amazingly Neat titles'
+
+def convert_string_to_title(s):
+    """
+    :param s: string of any length
+    :return: capitlised string with first letter in uppercase, rest letters in lower case.
+    """
+
+
+
+# Define tests
+
+class TestTitleFunction(TestCase):
+
+    def test_converts_to_title(self):
+        s = 'we lOve to hAVe really amazingly Neat titles'
+        response = convert_string_to_title(s)
+        expected_response = s.title()
+        self.assertEqual(expected_response, response)\
+
+    def test_converts_including_question_mark(self):
+        s = 'may the fOrTh be with you - obi?wan kenobi'
+        response = convert_string_to_title(s)
+        expected_response = s.title()
+        self.assertEqual(expected_response, response)
+
+    def test_converts_including_random(self):
+        s = "that's the beSt damn españOl lesson I've #seenthisweek"
+        response = convert_string_to_title(s)
+        expected_response = s.title()
+        self.assertEqual(expected_response, response)
